55082587e6333ce6a97f581b172a37e365b18af6
Erik Baldwin <erik.baldwin@acquia.com>
2015-02-19 16:32:16 -0800

    Added initial gulp file with a couple tasks in it:   - Styles: takes all scss and sass files in our source dir and runs them through ruby-sass to compile and then save to distribution dir   - Scripts: concatenates all script files in our source dir and outputs them as one file in the distribution dir

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Package Requirements
+var gulp        = require('gulp'),
+    rubySass    = require('gulp-ruby-sass'), // Ruby-Sass because reasons ...
+    autoPrefix  = require('gulp-autoprefixer'),
+    concat      = require('gulp-concat');
+
+// Let's create a task that processes our sass and scss files.
+gulp.task('styles', function () {
+  // rather than passing in an object of sassSettings directly into the fn call
+  // we'll add it via reference to a variable.
+  var sassSettings = {
+    loadPath: [
+      './src/styles'
+    ],
+    require: 'compass/import-once/activate',
+    style: 'expanded',
+    trace: true,
+    unixNewlines: true
+  };
+  return rubySass('./src/styles', sassSettings)
+    .on('error', function (err) {
+      console.error('Error', err.message);
+    })
+    .pipe(autoPrefix())
+    .pipe(gulp.dest('./dist/styles'))
+});
+
+gulp.task('scripts', function () {
+  // Now we need to concat all of our script files into one
+  gulp.src('./src/scripts/**/*.js')
+    .pipe(concat('my-scripts.js'))
+    .pipe(gulp.dest('./dist/scripts'));
+});
